861ad6cb7ed70f846ec8be7d1584545c6036bd50
Mihael Stormrage <mihan.msn@hotmail.com>
2020-06-26 12:12:41 +0700

    Stage #1: Primitive Art

---
+++ src/signature/Main.kt
@@ -0,0 +1,8 @@
+package signature
+
+fun main() {
+    val name = "Hyper Skill"
+    println(" ${"_".repeat(name.length + 2)}")
+    println("| $name |")
+    println(" ${"¯".repeat(name.length + 2)}")
+}
